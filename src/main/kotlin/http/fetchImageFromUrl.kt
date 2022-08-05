06743db7e61a0872a0aaecad1967eb3f0a273062
Chufretalas <socorrobiptbopt@gmail.com>
2022-08-04 22:59:26 -0300

    the poster image is now fetched from the network by an URL

---
+++ src/main/kotlin/http/fetchImageFromUrl.kt
@@ -0,0 +1,22 @@
+package http
+
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import org.jetbrains.skia.Image
+import java.io.ByteArrayOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.imageio.ImageIO
+
+fun fetchImageFromUrl(url: URL): ImageBitmap {
+    val connection = url.openConnection() as HttpURLConnection
+    connection.connect()
+    val inputStream = connection.inputStream
+    val bufferedImage = ImageIO.read(inputStream)
+
+    val stream = ByteArrayOutputStream()
+    ImageIO.write(bufferedImage, "jpg", stream)
+    val byteArray = stream.toByteArray()
+
+    return Image.makeFromEncoded(byteArray).toComposeImageBitmap()
+}
